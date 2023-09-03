438057ecac3b594c0060c9a22097f0dc0797e173
Lnetface <104566775+abbcccbba@users.noreply.github.com>
2023-09-03 17:56:16 +0800

    测试提交

--- AppRuntime/FileIOD.cs
+++ AppRuntime/FileIOD.cs
@@ -15,6 +15,7 @@ namespace AppRuntime
     写入： new FileIOD("文件名.txt").xie("这里可以写需要写的内容");
     读取： string 读取的结果 = await new FileIOD("文件名").du();
      */
+
     public sealed class FileIOD
     {
              
