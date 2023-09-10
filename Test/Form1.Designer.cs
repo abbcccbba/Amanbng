7096bb590055438cc16561ee2858ce50654b93d0
Lnetface <104566775+abbcccbba@users.noreply.github.com>
2023-09-10 09:58:43 +0800

    第一个提交：为了适配修改为了win32.改了几乎所有文件

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Box
+            // 
+            this.Box.Image = global::Test.Properties.Resources._61c706ccafb5fbbde5a587e224ed241e;
+            this.Box.Location = new System.Drawing.Point(-1, -2);
+            this.Box.Name = "Box";
+            this.Box.Size = new System.Drawing.Size(857, 485);
+            this.Box.TabIndex = 0;
+            this.Box.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 477);
+            this.Controls.Add(this.Box);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "牛逼工具箱";
+            ((System.ComponentModel.ISupportInitialize)(this.Box)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Box;
+    }
+}
+
