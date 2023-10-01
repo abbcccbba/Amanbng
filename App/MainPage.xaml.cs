8b203a022229327aa066e1d119cfa998decc9a9e
Lnetface <104566775+abbcccbba@users.noreply.github.com>
2023-10-01 20:38:52 +0800

    进行了一些基础修改

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -1,27 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.Storage;
-using AppClasses;
-using Windows.Media.Playback;
-using Windows.Devices.Lights;
-using System.Threading.Tasks;
-using Windows.Web.Http;
-using Windows.Storage.Streams;
-using System.Drawing;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
 
@@ -85,40 +66,12 @@ namespace App
         public MainPage()
         {
             this.InitializeComponent();
-            //AppClasses.dt.SetWindowSize(853, 480); // 设置窗口大小
+
             ApplicationView.GetForCurrentView().SetPreferredMinSize(new Windows.Foundation.Size(853, 480));
-            ApplicationView.GetForCurrentView().TryResizeView(new Windows.Foundation.Size(853, 480)); 
+            ApplicationView.GetForCurrentView().TryResizeView(new Windows.Foundation.Size(853, 480));
         }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            VarFile();
-        }
-
-        private async void VarFile()
-        {
-            try
-            {
-                StorageFile configFile = await (ApplicationData.Current.LocalFolder).GetFileAsync("One.iso");
-                if(await FileIO.ReadTextAsync(configFile) == "2bNe3kw")
-                    Frame.Navigate(typeof(Login));
-                else
-                {
-                    textBlock.Text = "无法完成验证";
-                }
-            }
-            catch (System.IO.IOException)
-            {
-                StorageFile configFile = await (ApplicationData.Current.LocalFolder).CreateFileAsync("One.iso", CreationCollisionOption.ReplaceExisting);
-                await FileIO.WriteTextAsync(configFile, "2bNe3kw");
-                IsOne();
-            }
-        }
-        private async void IsOne()
-        {
-            textBlock.Text = "正在为初次打开进行准备工作";
-            Mainringing.IsActive = true;
-            /*这里写当应用初次打开时的处理代码*/
-            //await Task.Delay(10000);
             Frame.Navigate(typeof(Login));
         }
     }
